970fffa1137ea165110f4395a90ace00d1c9a22e
Edgar <edga.alva@gmail.com>
2016-07-04 23:37:21 -0500

    Cambiar tamaño de la ventana de juego

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
+var w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0],
+    x = w.innerWidth || e.clientWidth || g.clientWidth,
+    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+
 var app = {
 	program_name: "Tactics",
-	game: new Phaser.Game(window.outerWidth, window.outerHeight, Phaser.AUTO, 'play_area')
+	game: new Phaser.Game(x, y, Phaser.AUTO, 'play_area')
 }
