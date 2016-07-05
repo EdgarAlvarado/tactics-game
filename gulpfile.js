53ece83df0ab03dfd331d4bc0e322596a5a06b08
Edgar <edga.alva@gmail.com>
2016-07-05 13:20:50 -0500

    [Prerelease] Bumped version number

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,15 @@ var gulp = require('gulp');
 var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var imagemin = require('gulp-imagemin');
 var sourcemaps = require('gulp-sourcemaps');
 var del = require('del');
+var runSequence = require('run-sequence');
+var conventionalChangelog = require('gulp-conventional-changelog');
+var conventionalGithubReleaser = require('conventional-github-releaser');
+var bump = require('gulp-bump');
+var gutil = require('gulp-util');
+var git = require('gulp-git');
+var fs = require('fs');
 
 var paths = {
   scripts: ['js/**/*.js'],
@@ -30,19 +36,80 @@ gulp.task('scripts', ['clean'], function() {
     .pipe(gulp.dest('build/js'));
 });
 
-// Copy all static images
-gulp.task('images', ['clean'], function() {
-  return gulp.src(paths.images)
-    // Pass in options to the task
-    .pipe(imagemin({optimizationLevel: 5}))
-    .pipe(gulp.dest('build/img'));
-});
-
 // Rerun the task when a file changes
 gulp.task('watch', function() {
   gulp.watch(paths.scripts, ['scripts']);
-  gulp.watch(paths.images, ['images']);
 });
 
-// The default task (called when you run `gulp` from cli)
-gulp.task('default', ['watch', 'scripts']);
+gulp.task('changelog', function () {
+  return gulp.src('CHANGELOG.md', {
+    buffer: false
+  })
+    .pipe(conventionalChangelog({
+      preset: 'angular' // Or to any other commit message convention you use.
+    }))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('github-release', function(done) {
+  conventionalGithubReleaser({
+    type: "oauth",
+    token: '2bfd8ae9a2d66f29ff47fdcb82cdd8e9baecdf1e' // change this to your own GitHub token or use an environment variable
+  }, {
+    preset: 'angular' // Or to any other commit message convention you use.
+  }, done);
+});
+
+gulp.task('bump-version', function () {
+// We hardcode the version change type to 'patch' but it may be a good idea to
+// use minimist (https://www.npmjs.com/package/minimist) to determine with a
+// command argument whether you are doing a 'major', 'minor' or a 'patch' change.
+  return gulp.src(['./bower.json', './package.json'])
+    .pipe(bump({type: "patch"}).on('error', gutil.log))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('commit-changes', function () {
+  return gulp.src('.')
+    .pipe(git.add())
+    .pipe(git.commit('[Prerelease] Bumped version number'));
+});
+
+gulp.task('push-changes', function (cb) {
+  git.push('origin', 'master', cb);
+});
+
+gulp.task('create-new-tag', function (cb) {
+  var version = getPackageJsonVersion();
+  git.tag(version, 'Created Tag for version: ' + version, function (error) {
+    if (error) {
+      return cb(error);
+    }
+    git.push('origin', 'master', {args: '--tags'}, cb);
+  });
+
+  function getPackageJsonVersion () {
+    // We parse the json file instead of using require because require caches
+    // multiple calls so the version number won't be updated
+    return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
+  };
+});
+
+gulp.task('release', function (callback) {
+  runSequence(
+    'scripts',
+    'bump-version',
+    'changelog',
+    'commit-changes',
+    'push-changes',
+    'create-new-tag',
+    'github-release',
+    function (error) {
+      if (error) {
+        console.log(error.message);
+      } else {
+        console.log('RELEASE FINISHED SUCCESSFULLY');
+      }
+      callback(error);
+    });
+});
