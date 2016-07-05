8bc21ae3899d49eaf99cb80a650fa466cf83eab1
Edgar <edga.alva@gmail.com>
2016-07-04 23:05:36 -0500

    Implementar un nuevo juego

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 var app = {
-	program_name: "Smart Box",
+	program_name: "Tactics",
+	bgColors = [0x62bd18, 0xff5300, 0xd21034, 0xff475c, 0x8f16b2, 0x588c7e, 0x8c4646],
+	holeWidthRange = [40, 280],
+	wallRange = [10, 70],
 	game: new Phaser.Game(1280, 720, Phaser.AUTO, 'play_area')
 }
--- js/state/load.js
+++ js/state/load.js
@@ -3,7 +3,10 @@ app.load = {
 }
 
 app.load.preload = function(){
-  app.game.load.image('player', 'assets/gfx/player.png');
+  //app.game.load.image('player', 'assets/gfx/player.png');
+  app.game.load.image("base", "assets/gfx/base.png");
+  app.game.load.image("square", "assets/gfx/square.png");
+  app.game.load.image("top", "assets/gfx/top.png");
 }
 
 app.load.create = function(){
