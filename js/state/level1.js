20874b444340801121533fbb072f853f976af78e
Edgar <edga.alva@gmail.com>
2016-07-04 23:20:18 -0500

    Fix de variable app

--- js/state/level1.js
+++ js/state/level1.js
@@ -3,99 +3,99 @@ app.level1 = {
 }
 
 app.level1.create = function(){
-  //app.game.stage.backgroundColor = '#000000';
+    //app.game.stage.backgroundColor = '#000000';
 
-  //this.player = new Player(640, 360);
-  //this.player.sub_create();
+    //this.player = new Player(640, 360);
+    //this.player.sub_create();
 
-  app.game.scale.pageAlignHorizontally = true;
-  app.game.scale.pageAlignVertically = true;
-  app.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-  var tintColor = app.load.bgColors[app.game.rnd.between(0, app.load.bgColors.length - 1)];
-  app.game.stage.backgroundColor = tintColor;
-  this.leftSquare = app.game.add.sprite(0, app.game.height, "base");
-  this.leftSquare.anchor.set(1, 1);
-  this.rightSquare = app.game.add.sprite(app.game.width, app.game.height, "base");
-  this.rightSquare.anchor.set(0, 1);
-  this.leftWall = app.game.add.sprite(0, app.game.height - this.leftSquare.height, "top");
-  this.leftWall.anchor.set(1, 1);
-  this.rightWall = app.game.add.sprite(app.game.width, app.game.height - this.rightSquare.height, "top");
-  this.rightWall.anchor.set(0, 1);
-  this.square = app.game.add.sprite(app.game.width / 2, - 400, "square");
-  this.square.anchor.set(0.5);
-  this.square.scale.setTo(0.2, 0.2);
-  this.updateLevel();
+    app.game.scale.pageAlignHorizontally = true;
+    app.game.scale.pageAlignVertically = true;
+    app.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    var tintColor = app.load.bgColors[app.game.rnd.between(0, app.load.bgColors.length - 1)];
+    app.game.stage.backgroundColor = tintColor;
+    this.leftSquare = app.game.add.sprite(0, app.game.height, "base");
+    this.leftSquare.anchor.set(1, 1);
+    this.rightSquare = app.game.add.sprite(app.game.width, app.game.height, "base");
+    this.rightSquare.anchor.set(0, 1);
+    this.leftWall = app.game.add.sprite(0, app.game.height - this.leftSquare.height, "top");
+    this.leftWall.anchor.set(1, 1);
+    this.rightWall = app.game.add.sprite(app.game.width, app.game.height - this.rightSquare.height, "top");
+    this.rightWall.anchor.set(0, 1);
+    this.square = app.game.add.sprite(app.game.width / 2, - 400, "square");
+    this.square.anchor.set(0.5);
+    this.square.scale.setTo(0.2, 0.2);
+    this.updateLevel();
 
 }
 
 app.level1.update = function(){
 
-  //this.player.sub_update();
+    //this.player.sub_update();
 }
 
 app.level1.updateLevel = function() {
     var holeWidth = app.game.rnd.between(app.load.holeWidthRange[0], app.load.holeWidthRange[1]);
     var wallWidth = app.game.rnd.between(app.load.wallRange[0], app.load.wallRange[1]);
     var leftSquareTween = app.game.add.tween(this.leftSquare).to({
-         x: (app.game.width - holeWidth) / 2
+        x: (app.game.width - holeWidth) / 2
     }, 500, Phaser.Easing.Cubic.Out, true);
     var rightSquareTween = app.game.add.tween(this.rightSquare).to({
-         x: (app.game.width + holeWidth) / 2
+        x: (app.game.width + holeWidth) / 2
     }, 500, Phaser.Easing.Cubic.Out, true);
     var leftWalltween = app.game.add.tween(this.leftWall).to({
-         x: (app.game.width - holeWidth) / 2 - wallWidth
+        x: (app.game.width - holeWidth) / 2 - wallWidth
     }, 500, Phaser.Easing.Cubic.Out, true);
     var rightWallTween = app.game.add.tween(this.rightWall).to({
-         x: (app.game.width + holeWidth) / 2 + wallWidth
+        x: (app.game.width + holeWidth) / 2 + wallWidth
     }, 500, Phaser.Easing.Cubic.Out, true);
     var squareTween = app.game.add.tween(this.square).to({
-         y: 150,
-         angle: 50
+        y: 150,
+        angle: 50
     }, 1000, Phaser.Easing.Cubic.Out, true);
     squareTween.onComplete.add(function(){
-         app.game.input.onDown.add(this.grow, this);
-         this.rotateTween = app.game.add.tween(this.square).to({
-              angle: 40
-         }, 300, Phaser.Easing.Linear.None, true, 0, -1, true)
+        app.game.input.onDown.add(this.grow, this);
+        this.rotateTween = app.game.add.tween(this.square).to({
+            angle: 40
+        }, 300, Phaser.Easing.Linear.None, true, 0, -1, true)
     }, this)
 }
 
 app.level1.grow = function(){
-     app.game.input.onDown.remove(this.grow, this);
-     app.game.input.onUp.add(this.stop, this);
-     this.growTween = app.game.add.tween(this.square.scale).to({
-          x: 1,
-          y: 1
-     }, 1500, Phaser.Easing.Linear.None, true);
+    app.game.input.onDown.remove(this.grow, this);
+    app.game.input.onUp.add(this.stop, this);
+    this.growTween = app.game.add.tween(this.square.scale).to({
+        x: 1,
+        y: 1
+    }, 1500, Phaser.Easing.Linear.None, true);
 }
 
 app.level1.stop = function(){
-     app.game.time.events.add(Phaser.Timer.SECOND * 2, function(){
-          app.game.state.start("PlayGame");
-     }, this);
-     app.game.input.onUp.remove(this.stop, this);
-     this.growTween.stop();
-     this.rotateTween.stop();
-     this.rotateTween = app.game.add.tween(this.square).to({
-          angle: 0
-     }, 300, Phaser.Easing.Cubic.Out, true);
-     if(this.square.width <= this.rightSquare.x - this.leftSquare.x){
-          this.rotateTween.onComplete.add(function(){
-               this.fallTween = app.game.add.tween(this.square).to({
-                    y: app.game.height + this.square.height
-               }, 600, Phaser.Easing.Cubic.Out, true);
-          }, this);
-     }
-     else{
-          this.square.anchor.set(0.5, 1);
-          if(this.square.width <= this.rightWall.x - this.leftWall.x){
-               var destY = app.game.height - this.leftSquare.height;
-          }
-          else{
-               var destY = app.game.height - this.leftSquare.height - this.leftWall.height;
-          }
-          app.game.add.tween(this.square).to({
-               y: app.game.height - this.leftSquare.height
-          }, 600, Phaser.Easing.Bounce.Out, true);
-     }
+    app.game.time.events.add(Phaser.Timer.SECOND * 2, function(){
+        app.game.state.start("PlayGame");
+    }, this);
+    app.game.input.onUp.remove(this.stop, this);
+    this.growTween.stop();
+    this.rotateTween.stop();
+    this.rotateTween = app.game.add.tween(this.square).to({
+        angle: 0
+    }, 300, Phaser.Easing.Cubic.Out, true);
+    if(this.square.width <= this.rightSquare.x - this.leftSquare.x){
+        this.rotateTween.onComplete.add(function(){
+            this.fallTween = app.game.add.tween(this.square).to({
+                y: app.game.height + this.square.height
+            }, 600, Phaser.Easing.Cubic.Out, true);
+        }, this);
+    }
+    else{
+        this.square.anchor.set(0.5, 1);
+        if(this.square.width <= this.rightWall.x - this.leftWall.x){
+            var destY = app.game.height - this.leftSquare.height;
+        }
+        else{
+            var destY = app.game.height - this.leftSquare.height - this.leftWall.height;
+        }
+        app.game.add.tween(this.square).to({
+            y: app.game.height - this.leftSquare.height
+        }, 600, Phaser.Easing.Bounce.Out, true);
+    }
 }
--- js/state/load.js
+++ js/state/load.js
@@ -1,16 +1,17 @@
 app.load = {
-	bgColors = [0x62bd18, 0xff5300, 0xd21034, 0xff475c, 0x8f16b2, 0x588c7e, 0x8c4646];
-	holeWidthRange = [40, 280];
-	wallRange = [10, 70];
+
 }
 
 app.load.preload = function(){
-  //app.game.load.image('player', 'assets/gfx/player.png');
-  app.game.load.image("base", "assets/gfx/base.png");
-  app.game.load.image("square", "assets/gfx/square.png");
-  app.game.load.image("top", "assets/gfx/top.png");
+    //app.game.load.image('player', 'assets/gfx/player.png');
+    app.game.load.image("base", "assets/gfx/base.png");
+    app.game.load.image("square", "assets/gfx/square.png");
+    app.game.load.image("top", "assets/gfx/top.png");
+    bgColors = [0x62bd18, 0xff5300, 0xd21034, 0xff475c, 0x8f16b2, 0x588c7e, 0x8c4646];
+    holeWidthRange = [40, 280];
+    wallRange = [10, 70];
 }
 
 app.load.create = function(){
-  app.game.state.start('level1');
+    app.game.state.start('level1');
 }
