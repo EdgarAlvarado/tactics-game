a1a0952c98c7dcfa92149501c94d6947e2fc4a95
Edgar <edga.alva@gmail.com>
2016-07-04 23:22:31 -0500

    Fix de variable app

--- js/state/load.js
+++ js/state/load.js
@@ -7,9 +7,9 @@ app.load.preload = function(){
     app.game.load.image("base", "assets/gfx/base.png");
     app.game.load.image("square", "assets/gfx/square.png");
     app.game.load.image("top", "assets/gfx/top.png");
-    bgColors = [0x62bd18, 0xff5300, 0xd21034, 0xff475c, 0x8f16b2, 0x588c7e, 0x8c4646];
-    holeWidthRange = [40, 280];
-    wallRange = [10, 70];
+    this.bgColors = [0x62bd18, 0xff5300, 0xd21034, 0xff475c, 0x8f16b2, 0x588c7e, 0x8c4646];
+    this.holeWidthRange = [40, 280];
+    this.wallRange = [10, 70];
 }
 
 app.load.create = function(){
