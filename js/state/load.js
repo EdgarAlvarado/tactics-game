1c797c4afcac49016759bfd97a174398961265d0
Edgar <edga.alva@gmail.com>
2016-07-04 23:56:27 -0500

    Añadir niveles

--- js/state/load.js
+++ js/state/load.js
@@ -7,7 +7,7 @@ app.load.preload = function(){
     app.game.load.image("base", "assets/gfx/base.png");
     app.game.load.image("square", "assets/gfx/square.png");
     app.game.load.image("top", "assets/gfx/top.png");
-    game.load.bitmapFont("font", "assets/fonts/font.png", "assets/fonts/font.fnt");
+    app.game.load.bitmapFont("font", "assets/fonts/font.png", "assets/fonts/font.fnt");
 
     this.bgColors = [0x62bd18, 0xff5300, 0xd21034, 0xff475c, 0x8f16b2, 0x588c7e, 0x8c4646];
     this.holeWidthRange = [40, 280];
