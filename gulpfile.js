03d7ef49aa74c1cb39beb2cb1e934282fff0dc78
Edgar <edga.alva@gmail.com>
2016-07-06 10:32:57 -0500

    [Prerelease] Bumped version number

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ var git = require('gulp-git');
 var fs = require('fs');
 
 var paths = {
-  scripts: ['dev/**/*.js'],
+  scripts: ['dev/lib/phaser.js', 'dev/*.js', 'dev/actor/*.js', 'dev/state/*.js'],
   images: 'client/img/**/*'
 };
 
